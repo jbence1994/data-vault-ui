1454c6d93527ddc86b94a5e8b7ab78dc280a8faa
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-01 16:57:47 +0200

    Add initial landing page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import App from "./components/App";
+
+import { APPLICATION_NAME } from "./constants/app.constants";
+
+import "./main.css";
+
+const root: ReactDOM.Root = ReactDOM.createRoot(
+  document.getElementById("root")!,
+);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <App applicationName={APPLICATION_NAME} />
   </React.StrictMode>,
 );
