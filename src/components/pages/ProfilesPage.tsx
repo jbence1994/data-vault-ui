8bdcb8b5e48baaca062e4102e1ea3b482c9df829
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-02 12:32:59 +0200

    Render profiles into a table component.

--- src/components/pages/ProfilesPage.tsx
+++ src/components/pages/ProfilesPage.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
 
-const ProfilesPage: React.FC<PageProps> = ({ pageName }) => {
+import ProfileTable from "../custom/ProfileTable.tsx";
+
+import PROFILES from "../../graphql/queries/profilesQuery.ts";
+
+import { NETWORK_ONLY } from "../../constants/app.constants.ts";
+
+const ProfilesPage: React.FC<PageProps> = () => {
+  const [profiles, setProfiles] = useState<Profile[]>([]);
+
+  const { loading, error, data } = useQuery(PROFILES, {
+    fetchPolicy: NETWORK_ONLY,
+  });
+
+  useEffect(() => {
+    if (!loading && !error) {
+      setProfiles(data.profiles);
+    }
+  }, [loading, error, data]);
+
+  const profileTableColumns = [
+    { key: "id", value: "Identifier" },
+    { key: "firstName", value: "First name" },
+    { key: "middleName", value: "Middle name" },
+    { key: "lastName", value: "Last name" },
+    { key: "createdDateTime", value: "Created date time" },
+  ];
+
   return (
     <section className="row">
-      <article className="col-12">
-        <div className="text-center">
-          <h1>{pageName}</h1>
-        </div>
-      </article>
+      {profiles.map((profile: Profile) => (
+        <article key={profile.id} className="col-sm-12 col-md-12 col-lg-12">
+          <ProfileTable columns={profileTableColumns} profiles={profiles} />
+        </article>
+      ))}
     </section>
   );
 };
--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -24,6 +24,27 @@ interface MainPageProps extends PageProps {
   applicationName: string;
 }
 
+interface ProfileTableProps {
+  columns: KeyValuePair<string, string>[];
+  profiles: Profile[];
+}
+
+// Custom data model types
+
+interface Profile {
+  [key: string]: number | string;
+  id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  gender: number;
+  birthDate: string;
+  phone: string;
+  email: string;
+  photo: string;
+  createdDateTime: string;
+}
+
 // Other custom types
 
 interface KeyValuePair<K, V> {
