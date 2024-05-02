f500f091d54119cc03b40fc363ac683c8bc7117f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-02 12:32:59 +0200

    Render profiles into a table component.

--- src/components/pages/ProfilesPage.tsx
+++ src/components/pages/ProfilesPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
+import ProfileTable from "../custom/ProfileTable.tsx";
+
 import PROFILES from "../../graphql/queries/profilesQuery.ts";
 
 import { NETWORK_ONLY } from "../../constants/app.constants.ts";
@@ -18,13 +20,20 @@ const ProfilesPage: React.FC<PageProps> = () => {
     }
   }, [loading, error, data]);
 
+  const profileTableColumns = [
+    { key: "id", value: "Identifier" },
+    { key: "firstName", value: "First name" },
+    { key: "middleName", value: "Middle name" },
+    { key: "lastName", value: "Last name" },
+    { key: "createdDateTime", value: "Created date time" },
+  ];
+
   return (
     <section className="row">
       {profiles.map((profile: Profile) => (
-        <article
-          key={profile.id}
-          className="col-sm-12 col-md-12 col-lg-12"
-        ></article>
+        <article key={profile.id} className="col-sm-12 col-md-12 col-lg-12">
+          <ProfileTable columns={profileTableColumns} profiles={profiles} />
+        </article>
       ))}
     </section>
   );
