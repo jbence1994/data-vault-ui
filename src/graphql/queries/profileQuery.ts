b647e9b5b7e7a98670590e34c61860ca15f0adf9
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-03 20:54:06 +0200

    Add profile by id GraphQL query

---
+++ src/graphql/queries/profileQuery.ts
@@ -0,0 +1,21 @@
+import { DocumentNode } from "graphql/language";
+import { gql } from "@apollo/client";
+
+const PROFILE: DocumentNode = gql`
+  query profile($id: String!) {
+    profile(id: $id) {
+      id
+      firstName
+      middleName
+      lastName
+      gender
+      birthDate
+      phone
+      email
+      photo
+      createdDateTime
+    }
+  }
+`;
+
+export default PROFILE;
